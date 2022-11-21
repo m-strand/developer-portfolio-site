bda896f375c75dbe9f47132617faaa983d00bc5e
Brooke <brooke.strand007@gmail.com>
2022-11-20 21:06:54 -0500

    adjusted structure of skill comp + removed obsolete projectcard comp

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,4 @@
 import React from "react";
-import SkillsCard from "./SkillsCard.js";
 import CSSskill from "./resources/css_skill.png";
 import HTMLskill from "./resources/html_skill.png";
 import JSskill from "./resources/javascript_skill.png";
@@ -39,11 +38,30 @@ function Skills () {
         <div id="Skills" className="skillsContainer">
             <h2 className="section"><span className="section-text">SKILLS</span></h2>
             <div className="skillCardSection">
-            {skillsArr.map((skill) => {
-                return (
-                    <SkillsCard skillImg={skill.img} skillName={skill.name} />
-                )
-            })}
+              <div className="skillCard">
+                <img src={HTMLskill} className="skillImg"/>
+                <p className="skillName">HTML</p>
+              </div>
+              <div className="skillCard">
+                <img src={CSSskill} className="skillImg"/>
+                <p className="skillName">CSS</p>
+              </div>
+              <div className="skillCard">
+                <img src={JSskill} className="skillImg"/>
+                <p className="skillName">Javascript</p>
+              </div>
+              <div className="skillCard">
+                <img src={ReactSkill} className="skillImg"/>
+                <p className="skillName">React</p>
+              </div>
+              <div className="skillCard">
+                <img src={SQLskill} className="skillImg"/>
+                <p className="skillName">SQL</p>
+              </div>
+              <div className="skillCard">
+                <img src={BootstrapSkill} className="skillImg"/>
+                <p className="skillName">Bootstrap</p>
+              </div>
             </div>
         </div>
     );
